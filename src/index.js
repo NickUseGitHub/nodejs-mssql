861087e04e0fdc1b79a08744c9f10af64a2c9760
Chaiwtun <chaiwtun@scg.com>
2020-03-01 00:55:10 +0700

    [prettier] install lint & prettier

--- src/index.js
+++ src/index.js
@@ -1,55 +1,58 @@
-const express = require('express');
-const { Connection, Request } = require('tedious');
+const express = require('express')
+const { Connection, Request } = require('tedious')
 
-const port = 3000;
-const app = express();
+const port = 3000
+const app = express()
 
 const config = {
   server: 'localhost',
-	authentication: {
+  authentication: {
     type: 'default',
     options: {
       userName: 'sa',
-      password: 'Pass_12345'
-    }
-  }
-};
+      password: 'Pass_12345',
+    },
+  },
+}
 
 const connection = new Connection(config)
 
-connection.on('connect', function (err) {
+connection.on('connect', function(err) {
   if (err) {
-    console.log(err);
+    console.log(err)
   } else {
-    console.log('Sql Server connected!');
+    console.log('Sql Server connected!')
   }
 })
 
 function findAll(request) {
   return new Promise(async function(resolve, reject) {
-    await connection.execSql(request);
+    await connection.execSql(request)
 
-    request.on('row', resolve);
-    request.on('error', reject);
-  });
+    request.on('row', resolve)
+    request.on('error', reject)
+  })
 }
 
-app.get('/', async function (_, res) {
-  const request = new Request("select * from nodedb.dbo.Todo", function (err, rowCount) {
+app.get('/', async function(_, res) {
+  const request = new Request('select * from nodedb.dbo.Todo', function(
+    err,
+    rowCount
+  ) {
     if (err) {
-      console.log(err);
+      console.log(err)
     } else {
-      console.log(rowCount + ' rows');
+      console.log(rowCount + ' rows')
     }
   })
-  
-  const data = await findAll(request);
 
-  console.log('data', data);
+  const data = await findAll(request)
 
-  res.send("hello world");
-});
+  console.log('data', data)
 
-const server = app.listen(3000, function () {
-    console.log(`Server is running ... on port: ${port}`);
-});
+  res.send('hello world')
+})
+
+const server = app.listen(3000, function() {
+  console.log(`Server is running ... on port: ${port}`)
+})
